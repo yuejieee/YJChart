a60cfda7e30f6fd5633d7c2475accc9202e38c57
Yue Jie <yuejieee@gmail.com>
2017-11-29 18:14:53 +0800

    Update YJLineChart.h

--- YJChart/YJChart/YJLineChart.h
+++ YJChart/YJChart/YJLineChart.h
@@ -2,18 +2,26 @@
 //  YJLineChart.h
 //  YJChart
 //
-//  Created by Kingpin on 2017/4/18.
+//  Created by yuejieee on 2017/4/18.
 //  Copyright © 2017年 yuejieee. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface YJLineChart : UIView
+/**
+ x轴数组
+ */
+@property (nonatomic, strong) NSArray *xValueArray;
+/**
+ y轴数组
+ */
+@property (nonatomic, strong) NSArray *yValueArray;
+/**
+ 数据数组
+ */
+@property (nonatomic, strong) NSArray *dataArray;
 
-- (void)setXValuesWithArray:(NSArray *)array;
-
-- (void)setYValuesWithArray:(NSArray *)array;
-
-- (void)setDataArray:(NSArray *)array;
+- (instancetype)initWithFrame:(CGRect)frame xValueArray:(NSArray *)xValueArray yValueArray:(NSArray *)yValueArray dataArray:(NSArray *)dataArray;
 
 @end
